350789e42db3aed8ce242bee7bc402119c72d825
Andie B <thepatrickbush@outlook.com>
2025-12-17 13:31:17 -0700

    Update NavigationButtonPool.cs

--- Assets/Scripts/Navigation/NavigationButtonPool.cs
+++ Assets/Scripts/Navigation/NavigationButtonPool.cs
@@ -1,22 +1,54 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class NavigationButtonPool : MonoBehaviour
 {
     [SerializeField] private GameObject navigationButtonPrefab;
-    [SerializeField] private Transform parent;
+    [SerializeField] private Transform defaultParent;
     [SerializeField] private int buttonPoolSize = 10;
     
-    private List<GameObject> buttons = new List<GameObject>();
-
+    //private List<GameObject> buttons = new List<GameObject>();
+    private readonly Queue <GameObject> buttons = new Queue<GameObject>();
+    private readonly HashSet<GameObject> activeButtons = new HashSet<GameObject>();
+    
     private void Awake()
     {
         //instantiate all buttons and add to object pool
         for (int i = 0; i < buttonPoolSize; i++)
         {
-            GameObject button = Instantiate(navigationButtonPrefab, parent.transform);
+            GameObject button = Instantiate(navigationButtonPrefab, defaultParent);
             button.SetActive(false);
-            buttons.Add(button);
+            buttons.Enqueue(button);
         }
     }
+
+    public GameObject GetButton(Transform parent)
+    {
+        GameObject button = buttons.Dequeue();
+        button.transform.SetParent(parent);
+        activeButtons.Add(button);
+        return button;
+    }
+
+    public void ReleaseButton(GameObject button)
+    {
+        //return to pool
+        activeButtons.Remove(button);
+        buttons.Enqueue(button);
+
+        //clear previous navigation assignment
+        var listener = button.GetComponent<Button>();
+        if (listener != null) listener.onClick.RemoveAllListeners();
+
+        //deactivate and remove from parent
+        button.SetActive(false);
+        button.transform.SetParent(defaultParent, false);
+    }
+
+    public void ReleaseAllButtons()
+    {
+        foreach (GameObject button in activeButtons) ReleaseButton(button);
+    }
+    
 }
